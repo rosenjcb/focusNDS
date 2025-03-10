bedf2da9795573515b794bbaaee4fa0df9a5fa3f
Jacob Rosenzweig <rosenjcb@gmail.com>
2025-03-09 17:50:22 -0700

    pushing change

--- community/themespec/theme_voucher/send-over.sh
+++ community/themespec/theme_voucher/send-over.sh
@@ -1,9 +1,13 @@
-scp -O backdrop.jpg root@10.0.0.2:/etc/opennds/htdocs/images # /images is wrong but I'm not sure if I want a backdrop. 
-scp -O location-logo.png root@10.0.0.2:/etc/opennds/htdocs/images
-scp -O focus.png root@10.0.0.2:/etc/opennds/htdocs/images
-scp -O backdrop.jpg root@10.0.0.2:/etc/opennds/htdocs/images
-scp -O splash-test.css root@10.0.0.2:/etc/opennds/htdocs
-scp -O phone-validation.js root@10.0.0.2:/etc/opennds/htdocs
-scp -O theme_voucher.sh root@10.0.0.2:/usr/lib/opennds
-scp -O ../../../forward_authentication_service/libs/libopennds.sh root@10.0.0.2:/usr/lib/opennds
-scp -O ../../../forward_authentication_service/libs/client_params.sh root@10.0.0.2:/usr/lib/opennds
+# Default IP
+DEST_IP=${1:-base-camp-wifi-1-ssh.gofocus.space}
+
+# Define file transfers
+scp -O backdrop.jpg root@$DEST_IP:/etc/opennds/htdocs/images # /images is wrong but I'm not sure if I want a backdrop.
+scp -O location-logo.png root@$DEST_IP:/etc/opennds/htdocs/images
+scp -O focus.png root@$DEST_IP:/etc/opennds/htdocs/images
+scp -O backdrop.jpg root@$DEST_IP:/etc/opennds/htdocs/images
+scp -O splash-test.css root@$DEST_IP:/etc/opennds/htdocs
+scp -O phone-validation.js root@$DEST_IP:/etc/opennds/htdocs
+scp -O theme_voucher.sh root@$DEST_IP:/usr/lib/opennds
+scp -O ../../../forward_authentication_service/libs/libopennds.sh root@$DEST_IP:/usr/lib/opennds
+scp -O ../../../forward_authentication_service/libs/client_params.sh root@$DEST_IP:/usr/lib/opennds
